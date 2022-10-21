8908a55902e70ea70af574f30767448f6901b931
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-10-21 18:59:30 +0800

    Add files via upload

---
+++ LabWork3/q6a.php
@@ -0,0 +1,12 @@
+<html> 
+<head> 
+<title>Loop - Display 1 to 10</title> 
+</head> 
+<body> 
+ <?php
+    for($i = 1; $i <= 10; $i++){
+        echo $i . '<br>';
+    }
+?> 
+</body> 
+</html>
---
+++ LabWork3/q6b.php
@@ -0,0 +1,12 @@
+<html> 
+<head> 
+<title>Loop - Display B</title> 
+</head> 
+<body> 
+ <?php
+    for($i = 1; $i <= 21; $i += 4) {
+        echo $i . '<br>';
+    }
+?> 
+</body> 
+</html>
